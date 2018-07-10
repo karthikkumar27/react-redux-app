387a2ceec2eb4c88160c4e29dce98dcdabdb2f2d
samuraijack <karthikkumar.r@gmail.com>
2018-07-10 12:20:33 +0800

    proj init strcture and settings

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './components/App';
+import './style.less';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+render(<App />, document.getElementById('container'));
+
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+      filename: "./index.html"
+    })
+    //new OpenBrowserPlugin({ url: 'http://localhost:8080' })
+  ]
+};
